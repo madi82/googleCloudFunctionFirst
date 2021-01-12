06a9da5dd40dc8d71fb53ba5007ff65b7a419c73
madi82 <shantanumadiwale@gmail.com>
2021-01-12 12:45:27 +0530

    remove file

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("../firestoreServiceAccountKey.json")
+cred = credentials.Certificate("../firestorServiceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
