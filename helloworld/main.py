054fa17eabf649debeff49d6b3f3d1c2aa771812
madi82 <shantanumadiwale@gmail.com>
2021-01-13 10:30:47 +0530

    update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("../firestorServiceAccountKey.json")
+cred = credentials.Certificate("../firestoreServiceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
